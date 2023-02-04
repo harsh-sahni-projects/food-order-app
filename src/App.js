afd75e94558d7dff3925bfe5e9619aeeb5261742
harsh-sahni-projects <harshsahni555@gmail.com>
2023-02-05 00:16:29 +0530

    adding support for fetching meals data from firebase realtime database

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ import CartProvider from "./store/CartProvider";
 
 function App() {
 
-  const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Farmhouse Pizza',
-      description: 'Onion, Capsicum, Grilled Mushroom & Tomato',
-      price: 23,
-    },
-    {
-      id: 'm2',
-      name: 'Double Cheese Margherita',
-      description: 'Loaded with extra cheese',
-      price: 18,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, premium veg',
-      price: 12,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 12,
-    },
-  ];
+  // const DUMMY_MEALS = [
+  //   {
+  //     id: 'm1',
+  //     name: 'Farmhouse Pizza',
+  //     description: 'Onion, Capsicum, Grilled Mushroom & Tomato',
+  //     price: 23,
+  //   },
+  //   {
+  //     id: 'm2',
+  //     name: 'Double Cheese Margherita',
+  //     description: 'Loaded with extra cheese',
+  //     price: 18,
+  //   },
+  //   {
+  //     id: 'm3',
+  //     name: 'Barbecue Burger',
+  //     description: 'American, raw, premium veg',
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 'm4',
+  //     name: 'Green Bowl',
+  //     description: 'Healthy...and green...',
+  //     price: 12,
+  //   },
+  // ];
 
   const [isCartVisible, setCartVisibility] = useState(false);
 
